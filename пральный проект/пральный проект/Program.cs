017066f51551fd2401917df917e6380848a30da9
Kekw <grishenkofedor@mail.ru>
2021-03-27 15:57:35 +0300

    add class point

--- пральный проект/пральный проект/Program.cs
+++ пральный проект/пральный проект/Program.cs
@@ -10,26 +10,29 @@ using System.Text;
 using System.Threading.Tasks;
 namespace пральный_проект
 {
-    class Program
+
+    class Point
     {
-        static void Main(string[] args)
-        {
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
-            Draw(x1, y1, sym1);
-
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
-            Draw(x2, y2, sym2);
-
-        }
-        static void Draw(int x,int y,char sym)
+        public int x;
+        public int y;
+        public char sym;
+        public void Draw(int x, int y, char sym)
         {
             Console.SetCursorPosition(x, y);
             Console.WriteLine(sym);
         }
 
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point();
+            p1.Draw(1, 3, '*');
+
+            Point p2 = new Point();
+            p2.Draw(4, 5, '#');
+        }
     }
 }
