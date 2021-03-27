3cf72bb2bc9e3e6c30c8e3e69b8a0a2d5b14df7a
Kekw <grishenkofedor@mail.ru>
2021-03-27 15:09:37 +0300

    create project

---
+++ пральный проект/пральный проект/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+namespace пральный_проект
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
